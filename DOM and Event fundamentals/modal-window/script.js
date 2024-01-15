fac9a2ae18398ef0325f358b6049d476931fb470
NathanaelTamirat <natnaeltamirat12@gmail.com>
2024-01-15 13:42:00 +0800

    modal window project

--- DOM and Event fundamentals/modal-window/script.js
+++ DOM and Event fundamentals/modal-window/script.js
@@ -1,1 +1,36 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const close = document.querySelector('.close-modal');
+const btnShowModal = document.querySelectorAll('.show-modal');
+// console.log(btnShowModal); /// to show the node
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnShowModal.length; i++) {
+  btnShowModal[i].addEventListener('click', openModal);
+}
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+close.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+///////another way using style
+// for (let i = 0; i < btnShowModal.length; i++) {
+//   console.log(
+//     btnShowModal[i].addEventListener('click', function () {
+//       document.querySelector('.hidden').style.display = 'block';
+//     })
+//   );
+// }
+
+// document.querySelector('.close-modal').addEventListener('click', function () {
+//   document.querySelector('.hidden').style.display = 'none';
+// });
